5e06fbbdc2708a04151b3bfe3152c18f254d0c3d
Sethum Vidmal <sethumthabrew@gmail.com>
2023-12-07 12:43:42 +0530

    Initial commit

---
+++ src/app/components/call-popup/call-popup.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-call-popup',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './call-popup.component.html',
+  styleUrl: './call-popup.component.css'
+})
+export class CallPopupComponent {
+
+}
