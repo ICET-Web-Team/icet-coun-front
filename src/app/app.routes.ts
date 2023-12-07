1a9fb4b2184939df88461443d89601464fb5796f
SUPUNI\supun <sandunsudara692gmail.com>
2023-12-07 16:59:58 +0530

    add componet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { InquireRegComponent } from './components/c&m/inquire-reg/inquire-reg.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path : 'reg',
+    component : InquireRegComponent
+}];
