ca206cef1213fcdc0f61578969fc745cc17266e6
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-12-11 12:06:43 +0530

    install chart.js

---
+++ src/app/components/manager/student_progress/chart-app/chart-app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chart-app',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chart-app.component.html',
+  styleUrl: './chart-app.component.css'
+})
+export class ChartAppComponent {
+
+}
