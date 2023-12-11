08abc943f2e9b0d2caeabaddb0c2f90508b40020
Sithum2002 <sithumasam@gmail.com>
2023-12-11 10:12:40 +0530

    Initialize sidebar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { ManagerDashComponent } from './pages/manager-dash/manager-dash.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'sasa',
+        component : ManagerDashComponent
+    }
+];
