f205d79dba9ec7129b6c79b04ed6d6e7f5b4c6b7
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-12-11 13:18:22 +0530

    create chart

--- src/app/components/manager/student_progress/chart-app/chart-app.component.ts
+++ src/app/components/manager/student_progress/chart-app/chart-app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Chart,registerables} from 'chart.js';
 
 @Component({
   selector: 'app-chart-app',
@@ -8,6 +9,53 @@ import { CommonModule } from '@angular/common';
   templateUrl: './chart-app.component.html',
   styleUrl: './chart-app.component.css'
 })
-export class ChartAppComponent {
+export class ChartAppComponent implements OnInit{
+  chart:any;
+  constructor(){}
+
+  ngOnInit(): void {
+    this.chart = document.getElementById('progress_chart');
+    Chart.register(...registerables);
+    this.loadChart();
+  }
+  loadChart() {
+    new Chart(this.chart, {
+      type:'line',
+      data:{
+        datasets: [{
+          data: [30,60,40,50,40,55,85,65,75,50,70,100],
+          label: 'Progress',
+          backgroundColor: '#fff',
+          tension:0.2,
+          borderColor:'#521164',
+        }],
+        labels: [
+          'Jan',
+          'Feb',
+          'Mar', 
+          'Apr', 
+          'May', 
+          'Jun', 
+          'Jul', 
+          'Aug', 
+          'Sep', 
+          'Oct', 
+          'Nov',
+          'Des',
+        ],
+      },
+    options:{
+      responsive:true,
+      maintainAspectRatio: false,
+      scales:{
+        y:{
+          beginAtZero: true,
+        }
+      }
+    }
+     });
+  }
 
 }
+
+
