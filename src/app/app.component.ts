71ca3b9a6293401114216e9312925a829731809d
Senalee Visal <m.m.senaleevisal@gmail.com>
2023-12-11 11:33:23 +0530

    counsellor registation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CounsellorRegComponent } from './components/manager/counsellor-reg/counsellor-reg.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CounsellorRegComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
