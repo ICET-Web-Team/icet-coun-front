6edfbaeaca08211cc7bd94ce3270d72fbcb41e13
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-12-11 16:07:32 +0530

    resole error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { CompletedProgressChartComponent } from "./components/manager/student_progress/completed-progress-chart/completed-progress-chart.component";
+import { ChartAppComponent } from './components/manager/student_progress/chart-app/chart-app.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, CompletedProgressChartComponent]
+    imports: [CommonModule, RouterOutlet, ChartAppComponent]
 })
 export class AppComponent {
   title = 'icet-coun-front';
