7e3ada3c28e961deef69953456f7f14c7eee0b2f
Sethum Vidmal <sethumthabrew@gmail.com>
2023-12-13 21:12:35 +0530

    Complete login page without background

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
-import { ManagerDashComponent } from './pages/manager-dash/manager-dash.component';
 import { ChartAppComponent } from './components/manager/student_progress/chart-app/chart-app.component';
+import { LoginComponent } from './pages/login/login.component';
 export const routes: Routes = [
     {
         path:'chart',
         component:ChartAppComponent 
+    },
+    {
+        path :'login',
+        component:LoginComponent
     }
 ];
