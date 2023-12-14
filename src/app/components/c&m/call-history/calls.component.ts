7cfbd44ec27ecbe7574d342f8f84c7d492318bc7
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-12-14 14:09:28 +0530

    create call history since start

--- src/app/components/c&m/call-history/calls.component.ts
+++ src/app/components/c&m/call-history/calls.component.ts
@@ -9,33 +9,5 @@ import { CommonModule } from '@angular/common';
   styleUrl: './calls.component.css'
 })
 export class CallsComponent {
-  // headers = ["Name", "Type", "Duration", "Date"];
-
-  // rows: { [key: string]: string }[] = [
-  //   {
-  //     "Name": "Hasindu Herath",
-  //     "Type": "Missed Call",
-  //     "Duration": "02s",
-  //     "Date": "09/02"
-  //   },
-  //   {
-  //     "Name": "Keshan Nandasena",
-  //     "Type": "Outgoing",
-  //     "Duration": "05s",
-  //     "Date": "09/02"
-  //   },
-  //   {
-  //     "Name": "Ashen Perera",
-  //     "Type": "Missed Call",
-  //     "Duration": "2s",
-  //     "Date": "09/02"
-  //   },
-  //   {
-  //     "Name": "Upeksha Swarnamali",
-  //     "Type": "Missed Call",
-  //     "Duration": "2s",
-  //     "Date": "09/02"
-  //   }
-  // ];
 
 }
