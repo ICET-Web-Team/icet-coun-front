815fa2ea1f9dc92a36f7a05c652b7285c931a9f5
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-12-14 17:47:39 +0530

    finally , table was create

--- src/app/components/c&m/call-history/calls.component.ts
+++ src/app/components/c&m/call-history/calls.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './calls.component.html',
   styleUrl: './calls.component.css'
 })
-export class CallsComponent {
+export class CallsComponent implements OnInit {
+  ngOnInit(){
 
+  }
+  img1="./assets/images/WhatsApp Image 2023-09-27 at 11.17.39.jpg";
+  iconMissed="./assets/images/icons8-missed-call-16.png";
 }
