d50ae3024aa38279e64723cf76c02bfc5993af82
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-12-19 14:57:03 +0530

    update card hover color and change name as counsellor name into the m-call-history

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
         component:ChartAppComponent 
     },
    {
-    path:'counsill-call-history',
+    path:'counsellor-call-history',
     component:CCallHistoryComponent
    },
    {
