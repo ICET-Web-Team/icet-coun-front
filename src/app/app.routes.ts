84e042d0b4ba1299bf90cf9e9523b66b3f80e080
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-12-19 11:30:29 +0530

    create call history component into the manager dash board

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ManagerDashComponent } from './pages/manager-dash/manager-dash.component';
 import { ChartAppComponent } from './components/manager/student_progress/chart-app/chart-app.component';
 import { CCallHistoryComponent } from './components/counsellor/c-call-history/c-call-history.component';
+import { MCallHistoryComponent } from './components/manager/m-call-history/m-call-history.component';
 
 export const routes: Routes = [
     {
@@ -9,7 +10,11 @@ export const routes: Routes = [
         component:ChartAppComponent 
     },
    {
-    path:'call-history',
+    path:'counsill-call-history',
     component:CCallHistoryComponent
+   },
+   {
+    path:'manager-call-history',
+    component:MCallHistoryComponent
    }
 ];
--- src/app/components/manager/m-call-history/m-call-history.component.ts
+++ src/app/components/manager/m-call-history/m-call-history.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './m-call-history.component.html',
   styleUrl: './m-call-history.component.css'
 })
-export class MCallHistoryComponent {
-
+export class MCallHistoryComponent implements OnInit{
+  ngOnInit() {
+   
+  }
+    img1="./assets/images/WhatsApp Image 2023-09-27 at 11.17.39.jpg";
+    iconMissed="./assets/images/icons8-missed-call-16.png";
 }
