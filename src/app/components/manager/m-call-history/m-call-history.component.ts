3e634f54fe8be3df0e57bb508e6a4276b9672518
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-12-19 13:36:31 +0530

    update manager call history table

--- src/app/components/manager/m-call-history/m-call-history.component.ts
+++ src/app/components/manager/m-call-history/m-call-history.component.ts
@@ -12,8 +12,10 @@ export class MCallHistoryComponent implements OnInit{
   ngOnInit() {
    
   }
-    img1="./assets/images/WhatsApp Image 2023-09-27 at 11.17.39.jpg";
-    img2="./assets/images/WhatsApp Image 2023-09-10 at 09.17.26.jpg";
-    img3="./assets/images/sha.jpg"
-    iconMissed="./assets/images/icons8-missed-call-16.png";
+  img1="./assets/images/WhatsApp Image 2023-09-27 at 11.17.39.jpg";
+  pic2="./assets/images/WhatsApp Image 2023-09-10 at 09.17.26.jpg";
+  img3="./assets/images/sha.jpg"
+  iconMissed="./assets/images/icons8-missed-call-16.png";
+  iconOuted="./assets/images/outgoing.png";
+  iconIncome="./assets/images/incoming.png"
 }
