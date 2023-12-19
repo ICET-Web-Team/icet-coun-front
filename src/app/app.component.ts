5f5bf47dc3645ffe07347ebd350ec734bd8e3fa5
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-12-19 15:23:38 +0530

    add sum part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,14 @@ import { RouterOutlet } from '@angular/router';
 
 import { ChartAppComponent } from './components/manager/student_progress/chart-app/chart-app.component';
 import { ManagerDashComponent } from "./pages/manager-dash/manager-dash.component";
+import { CounsellorRegComponent } from "./components/manager/counsellor-reg/counsellor-reg.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, ChartAppComponent, ManagerDashComponent]
-
+    imports: [CommonModule, RouterOutlet, ChartAppComponent, ManagerDashComponent, CounsellorRegComponent]
 })
 export class AppComponent {
   title = 'icet-coun-front';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { ManagerDashComponent } from './pages/manager-dash/manager-dash.component';
 import { ChartAppComponent } from './components/manager/student_progress/chart-app/chart-app.component';
 import { InquireRegComponent } from './components/c&m/inquire-reg/inquire-reg.component';
+import { CCallHistoryComponent } from './components/counsellor/c-call-history/c-call-history.component';
+import { MCallHistoryComponent } from './components/manager/m-call-history/m-call-history.component';
 export const routes: Routes = [
     {
         path:'chart',
         component:ChartAppComponent 
     },
-<<<<<<< HEAD
+
    {
     path:'counsellor-call-history',
     component:CCallHistoryComponent
@@ -16,10 +18,5 @@ export const routes: Routes = [
     path:'manager-call-history',
     component:MCallHistoryComponent
    }
-=======
-    {
-        path:'reg',
-        component : InquireRegComponent
-    }
->>>>>>> e0c77ec573fa91724b6ac3149e014a3711a355f3
+
 ];
