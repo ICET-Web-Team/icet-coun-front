54cd561f7c92ce6e4dc0184f87a8db319385115e
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2023-12-21 17:19:24 +0530

    create inquiry table and search area

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,24 +4,27 @@ import { InquireRegComponent } from './components/c&m/inquire-reg/inquire-reg.co
 import { MCallHistoryComponent } from './components/manager/m-call-history/m-call-history.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CounsellorRegComponent } from './components/manager/counsellor-reg/counsellor-reg.component';
+import { CInquiryListComponent } from './components/counsellor/c-inquiry-list/c-inquiry-list.component';
 
 export const routes: Routes = [
-    {
-        path:'chart',
-        component:ChartAppComponent 
-    },
-    {
-        path:'inquire-registration',
-        component : InquireRegComponent
-
-    },
-    {
-        path: 'call-history',
-        component : MCallHistoryComponent
-
-    },
-    {
-        path : 'counsellor-reg',
-        component : CounsellorRegComponent
-    }
+  {
+    path: 'chart',
+    component: ChartAppComponent,
+  },
+  {
+    path: 'inquire-registration',
+    component: InquireRegComponent,
+  },
+  {
+    path: 'call-history',
+    component: MCallHistoryComponent,
+  },
+  {
+    path: 'inquiry-list',
+    component: CInquiryListComponent,
+  },
+  {
+    path: 'counsellor-reg',
+    component: CounsellorRegComponent,
+  },
 ];
